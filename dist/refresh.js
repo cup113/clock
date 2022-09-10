35ec7e22a4f3e6cf5ba1282942371a54c27ea8c3
Jason M. Li <jasonmli@qq.com>
2022-09-10 21:20:28 +0800

    v2.0.3-release

--- dist/refresh.js
+++ dist/refresh.js
@@ -1,30 +1,33 @@
+"use strict";
 /**
  * @brief 若位数不够，则在前面补0
  * @param num 数字
  * @param digits 位数
  */
 function sup0(num, digits) {
-    var str = num.toString();
-    while (str.length < digits) {
-        str = '0' + str;
+    var s = num.toString();
+    if (s.length >= digits) {
+        return s;
     }
-    return str;
+    var ret = "";
+    for (var i = s.length; i < digits; ++i) {
+        ret += " ";
+    }
+    return ret + s;
 }
 var lastBodyWidth = 0, lastBodyHeight = 0, bodyWidth, bodyHeight, $second, $timer, bodyChanged, dateNow, hour, minute, second, secondWidth, lastHour, lastMinute;
+var $second = $("#second"), $timer = $("#timer");
 function refresh() {
-    $second = $("#second");
-    $timer = $("#timer");
-    bodyWidth = innerWidth;
-    bodyHeight = innerHeight;
-    bodyChanged = (bodyWidth !== lastBodyWidth) || (bodyHeight !== lastBodyHeight),
+    var bodyWidth = innerWidth;
+    bodyHeight = innerHeight,
         dateNow = new Date(),
         hour = dateNow.getHours(),
         minute = dateNow.getMinutes();
-    if (bodyChanged) {
+    if (innerWidth !== lastBodyWidth || bodyHeight !== lastBodyHeight) {
         var timerWidth = $timer.width(), timerHeight = $timer.height(), widthRatio = bodyWidth / timerWidth, heightRatio = bodyHeight / timerHeight, scale = Math.min(heightRatio, widthRatio), left, top;
         $timer.css("--scale", scale.toFixed(5));
-        left = (bodyWidth - $timer.width()) / 2;
-        top = (bodyHeight - $timer.height()) / 2;
+        left = (bodyWidth - timerWidth) / 2;
+        top = (bodyHeight - timerHeight) / 2;
         $timer.css("--top", top.toFixed(0));
         $timer.css("--left", left.toFixed(0));
         lastBodyWidth = bodyWidth;
@@ -37,6 +40,6 @@ function refresh() {
         lastMinute = minute;
         $timer.text("".concat(sup0(hour, 2), ":").concat(sup0(minute, 2)));
     }
-    setTimeout(refresh, 1000);
 }
 refresh();
+setInterval(refresh, 1000);
--- src/refresh.ts
+++ src/refresh.ts
@@ -4,52 +4,52 @@
  * @param digits 位数
  */
 function sup0(num: number, digits: number): string {
-	var str = num.toString();
-	while (str.length < digits) {
-		str = '0' + str;
-	}
-	return str;
+    var s = num.toString();
+    if (s.length >= digits) { return s; }
+    var ret = "";
+    for (let i = s.length; i < digits; ++i) { ret += " "; }
+    return ret + s;
 }
 
 var lastBodyWidth: number = 0, lastBodyHeight: number = 0,
-bodyWidth: number, bodyHeight: number,
-$second: JQuery<HTMLElement>, $timer: JQuery<HTMLElement>,
-bodyChanged: boolean, dateNow: Date,
-hour: number, minute: number, second: number,
-secondWidth: number, lastHour: number, lastMinute: number;
+    bodyWidth: number, bodyHeight: number,
+    $second: JQuery<HTMLElement>, $timer: JQuery<HTMLElement>,
+    bodyChanged: boolean, dateNow: Date,
+    hour: number, minute: number, second: number,
+    secondWidth: number, lastHour: number, lastMinute: number;
+
+var $second = $("#second"),
+    $timer = $("#timer");
 
 function refresh() {
-	$second = $("#second");
-	$timer = $("#timer");
-	bodyWidth = innerWidth;
-	bodyHeight = innerHeight;
-	bodyChanged = (bodyWidth !== lastBodyWidth) || (bodyHeight !== lastBodyHeight),
-	dateNow = new Date(),
-	hour = dateNow.getHours(),
-	minute = dateNow.getMinutes();
-	if (bodyChanged) {
-		var timerWidth = $timer.width(),
-		timerHeight = $timer.height(),
-		widthRatio = bodyWidth / timerWidth,
-		heightRatio = bodyHeight / timerHeight,
-		scale = Math.min(heightRatio, widthRatio),
-		left: number, top: number;
-		$timer.css("--scale", scale.toFixed(5));
-		left = (bodyWidth - $timer.width()) / 2;
-		top = (bodyHeight - $timer.height()) / 2;
-		$timer.css("--top", top.toFixed(0));
-		$timer.css("--left", left.toFixed(0));
-		lastBodyWidth = bodyWidth;
-		lastBodyHeight = bodyHeight;
-	}
-	second = dateNow.getSeconds() + dateNow.getMilliseconds() / 1000;
-	$second.css("--w", (second / 60).toFixed(5));
-	if (hour !== lastHour || minute !== lastMinute) {
-		lastHour = hour;
-		lastMinute = minute;
-		$timer.text(`${sup0(hour, 2)}:${sup0(minute, 2)}`);
-	}
-	setTimeout(refresh, 1000);
+    var bodyWidth = innerWidth;
+        bodyHeight = innerHeight,
+        dateNow = new Date(),
+        hour = dateNow.getHours(),
+        minute = dateNow.getMinutes();
+    if (innerWidth !== lastBodyWidth || bodyHeight !== lastBodyHeight) {
+        var timerWidth = $timer.width() as number,
+            timerHeight = $timer.height() as number,
+            widthRatio = bodyWidth / timerWidth,
+            heightRatio = bodyHeight / timerHeight,
+            scale = Math.min(heightRatio, widthRatio),
+            left: number, top: number;
+        $timer.css("--scale", scale.toFixed(5));
+        left = (bodyWidth - timerWidth) / 2;
+        top = (bodyHeight - timerHeight) / 2;
+        $timer.css("--top", top.toFixed(0));
+        $timer.css("--left", left.toFixed(0));
+        lastBodyWidth = bodyWidth;
+        lastBodyHeight = bodyHeight;
+    }
+    second = dateNow.getSeconds() + dateNow.getMilliseconds() / 1000;
+    $second.css("--w", (second / 60).toFixed(5));
+    if (hour !== lastHour || minute !== lastMinute) {
+        lastHour = hour;
+        lastMinute = minute;
+        $timer.text(`${sup0(hour, 2)}:${sup0(minute, 2)}`);
+    }
 }
 
 refresh();
+setInterval(refresh, 1000);
